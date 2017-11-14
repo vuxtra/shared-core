3b85b660f1d3606df94042aee0b9ae6121f1e566
Faruk Brbovic <fbrbovic@carqualifier.com>
2017-11-14 09:14:35 -0500

    fixed paths

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Request from './core/request'
+import Response from './core/response'
+import ServiceRequest from './core/serviceRequest'
+import ServiceResponse from './core/serviceResponse'
+
+export {
+    Request,
+    Response,
+    ServiceRequest,
+    ServiceResponse
+}
