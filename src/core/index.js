a5c3e07abb84c41052351d46d7c98beda28d1524
Faruk Brbovic <fbrbovic@carqualifier.com>
2017-11-14 09:00:32 -0500

    moved things around to make it more compatible and easier to include

---
+++ src/core/index.js
@@ -0,0 +1,11 @@
+import Request from './request'
+import Response from './response'
+import ServiceRequest from './serviceRequest'
+import ServiceResponse from './serviceResponse'
+
+export {
+    Request,
+    Response,
+    ServiceRequest,
+    ServiceResponse
+}
